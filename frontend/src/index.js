847ee83b8542118e7ad66b0d6b5568f6987bb0d9
Bosqy <groo@vth.ru>
2023-07-01 08:59:08 +1000

    fix bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@
 import ReactDOM from 'react-dom/client';
 import init from './init';
 
+import './assets/application.scss';
+
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(await init());
