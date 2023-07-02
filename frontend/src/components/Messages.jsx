17f2515009b8839794418b36a5d26bbead87f7a4
Bosqy <groo@vth.ru>
2023-07-02 15:35:42 +1000

    markup channels

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 const Messages = () => {
   const messages = useSelector((state) => state.messages);
   return (
-    <div>
+    <div className="col p-0 h-100">
       <pre>
         {JSON.stringify(messages)}
       </pre>
