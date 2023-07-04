652b0a313b1e4849d2b9dfbcf516f0a4db276d59
Bosqy <groo@vth.ru>
2023-07-04 15:13:24 +1000

    add dropdown menu

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -1,21 +1,58 @@
 import { useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, ButtonGroup, Dropdown } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 import { setChannel } from '../slices/channelsSlice';
 
 const Channel = ({ isActive, channel }) => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const handleSetChannel = (id) => dispatch(setChannel(id));
-  return (
-    <li className="nav-item w-100" key={channel.id}>
+  const handleRenameChannel = () => {};
+  const handleRemoveChannel = () => {};
+
+  const notRemovable = (
+    <Button
+      variant={isActive ? 'secondary' : ''}
+      className="w-100 rounded-0 text-start border-0"
+      onClick={() => handleSetChannel(channel.id)}
+    >
+      <span className="me-1">#</span>
+      {channel.name}
+    </Button>
+  );
+
+  const removable = (
+    <Dropdown as={ButtonGroup} className="d-flex">
       <Button
         variant={isActive ? 'secondary' : ''}
-        className="w-100 rounded-0 text-start"
+        className="w-100 rounded-0 text-start text-truncate border-0"
         onClick={() => handleSetChannel(channel.id)}
       >
         <span className="me-1">#</span>
         {channel.name}
       </Button>
+      <Dropdown.Toggle
+        split
+        variant={isActive ? 'secondary' : ''}
+        className="flex-grow-0 border-0"
+      >
+        <span className="visually-hidden">{t('modalManageChannel')}</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleRenameChannel}>
+          {t('modalRenameChannel')}
+        </Dropdown.Item>
+        <Dropdown.Item onClick={handleRemoveChannel}>
+          {t('modalRemoveChannel')}
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+
+  return (
+    <li className="nav-item w-100">
+      {channel.removable ? removable : notRemovable}
     </li>
   );
 };
